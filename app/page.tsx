c8bdde7f71cca53e645fa0cec209a35825999c5c
Ianina Nazarenko <iani.na.zarenko@gmail.com>
2025-08-02 15:42:46 +0300

    refactor: Clean HomePage from tailwind testing lines

--- app/page.tsx
+++ app/page.tsx
@@ -6,23 +6,6 @@ export default function Home() {
         <div className={'page'}>
             <div className={'container'}>
                 <h1 className={'h1'}>hello world!</h1>
-
-                <div className={'tabs'}>
-                    <TabGroup>
-                        <TabList>
-                            <Tab className="data-hover:underline data-selected:bg-blue-500 data-selected:text-white">
-                                Tab 1
-                            </Tab>
-                            <Tab>Tab 2</Tab>
-                            <Tab>Tab 3</Tab>
-                        </TabList>
-                    </TabGroup>
-                </div>
-
-                <div className="bg-blue-500 text-white p-4 rounded-lg shadow-md">
-                    <h1 className="text-2xl font-bold">Привет от Tailwind!</h1>
-                    <p className="mt-2">Эти стили должны быть применены.</p>
-                </div>
             </div>
         </div>
     );
