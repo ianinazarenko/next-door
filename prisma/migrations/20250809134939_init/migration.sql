6d326ce788b67af0bad356f585cba160a05452c0
Ianina Nazarenko <iani.na.zarenko@gmail.com>
2025-08-09 16:55:16 +0300

    feat: Redesign database schema and add new models
    - Add new models: `Category`, `Complex`, and `Comment`
    - Update `Post` model with additional fields and relations
    - Update Prisma migration and schema files

---
+++ prisma/migrations/20250809134939_init/migration.sql
@@ -0,0 +1,72 @@
+-- CreateTable
+CREATE TABLE "public"."Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "shortText" TEXT NOT NULL,
+    "fullText" TEXT NOT NULL,
+    "authorName" TEXT NOT NULL,
+    "phone" TEXT,
+    "whatsapp" TEXT,
+    "image" TEXT,
+    "deadline" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "complexId" TEXT NOT NULL,
+    "categoryId" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Complex" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "address" TEXT,
+    "metro" TEXT,
+    "description" TEXT,
+    "timeToMetro" INTEGER,
+
+    CONSTRAINT "Complex_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Comment" (
+    "id" TEXT NOT NULL,
+    "postId" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Post_categoryId_idx" ON "public"."Post"("categoryId");
+
+-- CreateIndex
+CREATE INDEX "Post_complexId_idx" ON "public"."Post"("complexId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Complex_slug_key" ON "public"."Complex"("slug");
+
+-- CreateIndex
+CREATE INDEX "Comment_postId_idx" ON "public"."Comment"("postId");
+
+-- AddForeignKey
+ALTER TABLE "public"."Post" ADD CONSTRAINT "Post_complexId_fkey" FOREIGN KEY ("complexId") REFERENCES "public"."Complex"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Post" ADD CONSTRAINT "Post_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "public"."Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "public"."Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
