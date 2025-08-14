5ff718544d2bf80c03ce0c97ba9e114c07c49160
Ianina Nazarenko <iani.na.zarenko@gmail.com>
2025-08-14 19:42:50 +0300

    refactor: Update styles
    - General typography & variables
    - Common UI components CButton, CDescription, CLabel

--- app/components/ui/CDescription.tsx
+++ app/components/ui/CDescription.tsx
@@ -1,7 +1,7 @@
 import { Description } from '@headlessui/react';
 
 function CDescription({ children }: { children?: React.ReactNode }) {
-    return <Description className="text-[1.2rem] text-(--text-secondary)">{children}</Description>;
+    return <Description className='text-[1.4rem] text-(--text-secondary)'>{children}</Description>;
 }
 
 export default CDescription;
--- app/components/ui/CLabel.tsx
+++ app/components/ui/CLabel.tsx
@@ -1,7 +1,7 @@
 import { Label } from '@headlessui/react';
 
 function CLabel({ children }: { children?: React.ReactNode }) {
-    return <Label className="text-[1.4rem] font-semibold text-(--text-primary)">{children}</Label>;
+    return <Label className='text-[1.6rem] font-semibold text-(--text-primary)'>{children}</Label>;
 }
 
 export default CLabel;
