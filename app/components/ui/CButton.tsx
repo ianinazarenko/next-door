521b11e77d33e9c76ed1cc30235a0e32a7a7ca31
Ianina Nazarenko <iani.na.zarenko@gmail.com>
2025-08-14 23:29:17 +0300

    style: Add active button state styles

--- app/components/ui/CButton.tsx
+++ app/components/ui/CButton.tsx
@@ -9,7 +9,7 @@ interface IButtonProps {
 
 function CButton({ type = 'button', disabled, onClick, children }: IButtonProps) {
     const classes =
-        'h-(--btn-height) rounded-(--radius-btn) bg-(--accent) px-(--p-btn) text-[1.4rem] font-semibold text-(--bg-primary) transition-colors hover:bg-(--accent-hover) lg:text-[1.4rem]';
+        'h-(--btn-height) rounded-(--radius-btn) bg-(--accent) px-(--p-btn) text-[1.4rem] font-semibold text-(--bg-primary) transition-colors hover:bg-(--accent-hover) lg:text-[1.4rem] active:bg-(--accent-hover)';
     return (
         <Button
             className={classes}
