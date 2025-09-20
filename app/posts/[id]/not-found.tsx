eb47bb0d0d3e8da03c7946d8dd6270f13d2f90ea
Ianina Nazarenko <iani.na.zarenko@gmail.com>
2025-09-20 18:36:06 +0300

    feat: Add NotFound page for Post page

--- app/posts/[id]/not-found.tsx
+++ app/posts/[id]/not-found.tsx
@@ -2,10 +2,10 @@ import { PAGES } from '@/utils/data/pages';
 import Link from 'next/link';
 import CButton from '@/app/components/ui/CButton';
 
-const BTN = 'Browse All Complexes';
-const TITLE = 'Complex Not Found';
-const TEXT = "Sorry, the complex you're looking for doesn't exist or has been moved.";
-export default function ComplexNotFound() {
+const BTN = 'Browse All Announcements';
+const TITLE = 'Announcement Not Found';
+const TEXT = "Sorry, the announcement you're looking for doesn't exist or has been moved.";
+export default function PostNotFound() {
     return (
         <div className={'page c-container flex flex-col items-center justify-center'}>
             <div className={'flex h-full flex-col items-center justify-center'}>
@@ -13,7 +13,7 @@ export default function ComplexNotFound() {
                 <p className={'card-description text-center'}>{TEXT}</p>
 
                 <Link
-                    href={PAGES.COMPLEXES.link}
+                    href={PAGES.POSTS.link}
                     className={'pt-4'}
                 >
                     <CButton>{BTN}</CButton>
