79c3f112d1d7b41d5b201c90de7d1d630d1da8b3
Ianina Nazarenko <iani.na.zarenko@gmail.com>
2025-09-27 11:33:30 +0300

    feat: Add CTextarea component

---
+++ app/components/ui/CTextarea.tsx
@@ -0,0 +1,31 @@
+import React, { ChangeEventHandler } from 'react';
+import { Field, Textarea as HeadlessTextarea } from '@headlessui/react';
+import CLabel from '@/app/components/ui/CLabel';
+import CDescription from '@/app/components/ui/CDescription';
+
+interface ITextareaProps {
+    label?: string;
+    description?: string;
+    placeholder?: string;
+    rows?: number;
+    onChange: ChangeEventHandler<HTMLTextAreaElement>;
+    className?: string;
+}
+
+function CTextarea({ onChange, label, description, placeholder, rows = 4, className = '', ...props }: ITextareaProps) {
+    return (
+        <Field className={`w-full ${className}`}>
+            {label && <CLabel>{label}</CLabel>}
+            {description && <CDescription>{description}</CDescription>}
+            <HeadlessTextarea
+                placeholder={placeholder}
+                rows={rows}
+                className={`mt-1 block min-h-[100px] w-full resize-y rounded-(--radius-input) border-none bg-(--bg-secondary) px-(--p-input-x) py-(--p-input-y) text-(--input-text) text-(--text-primary) focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-(--border)`}
+                onChange={onChange}
+                {...props}
+            />
+        </Field>
+    );
+}
+
+export default CTextarea;
