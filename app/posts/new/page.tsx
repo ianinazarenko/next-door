6ab33ebb5183f8c35b98eec9baa9b0cc01ba9790
Ianina Nazarenko <iani.na.zarenko@gmail.com>
2025-10-08 22:26:38 +0300

    feat: Change NewPost streaming

--- app/posts/new/page.tsx
+++ app/posts/new/page.tsx
@@ -3,34 +3,22 @@ import { PAGES_METADATA } from '@/utils/data/seo';
 // Types
 import { Metadata } from 'next';
 // Utils
-import { fetchCategoriesSpecs } from '@/lib/queries/categories';
-import { fetchComplexesSpecs } from '@/lib/queries/complexes';
 // Components
 import { Suspense } from 'react';
 import NewPostHeader from '@/app/components/pages/posts-new/NewPostHeader';
-import NewPostForm from '@/app/components/pages/posts-new/form/NewPostForm';
 import NewPostLoader from '@/app/components/pages/posts-new/NewPostLoader';
+import NewPostFormWrapper from '@/app/components/pages/posts-new/form/NewPostFormWrapper';
 
 export const metadata: Metadata = PAGES_METADATA.NEW_POST;
 
 export default async function NewPostPage() {
-    try {
-        const [categoriesSpecs, complexSpecs] = await Promise.all([
-            fetchCategoriesSpecs(false),
-            fetchComplexesSpecs(false),
-        ]);
+    return (
+        <div className={'page c-container'}>
+            <NewPostHeader />
 
-        return (
-            <div className={'page c-container'}>
-                <NewPostHeader />
-
-                <Suspense fallback={<NewPostLoader />}>
-                    <NewPostForm specs={{ complex: complexSpecs, category: categoriesSpecs }} />
-                </Suspense>
-            </div>
-        );
-    } catch (error) {
-        console.error('[NewPostPage]: Error loading specs:', error);
-        throw error;
-    }
+            <Suspense fallback={<NewPostLoader />}>
+                <NewPostFormWrapper />
+            </Suspense>
+        </div>
+    );
 }
