331c18d6f54850a15f43f7755d2ad5e0f5fc32a3
Ianina Nazarenko <iani.na.zarenko@gmail.com>
2025-10-16 21:33:06 +0300

    feat: Update Footer

--- app/components/common/footer/TheFooter.tsx
+++ app/components/common/footer/TheFooter.tsx
@@ -1,14 +1,42 @@
 import s from './TheFooter.module.css';
 
-export default function TheFooter() {
-    const DISCLAIMER = 'All rights reserved.';
-    const year = new Date().getFullYear();
+const year = new Date().getFullYear();
+const PROFILE_LINK = 'https://github.com/ianinazarenko';
+const PROJECT_LINK = 'https://github.com/ianinazarenko/next-door';
 
+const TEXT = {
+    disclaimer: 'Demo project by ',
+    name: 'Ianina Nazarenko',
+    viewSource: 'View source on ',
+    github: 'GitHub',
+};
+
+const PROPS = {
+    target: '_blank',
+    rel: 'noopener noreferrer',
+    className: s.link,
+};
+
+export default function TheFooter() {
     return (
         <footer className={s.footer}>
-            <div className={'c-container'}>
-                <p className={'card-description'}>
-                    &copy;&nbsp;{year}&nbsp;{DISCLAIMER}
+            <div className='c-container'>
+                <p className='card-description'>
+                    © {year} {TEXT.disclaimer}
+                    <a
+                        href={PROFILE_LINK}
+                        {...PROPS}
+                    >
+                        {TEXT.name}
+                    </a>
+                    <br />
+                    {TEXT.viewSource}
+                    <a
+                        href={PROJECT_LINK}
+                        {...PROPS}
+                    >
+                        {TEXT.github}
+                    </a>
                 </p>
             </div>
         </footer>
