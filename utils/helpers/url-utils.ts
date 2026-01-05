acac71fd52267bbe2bac9a52b533feedda2ee792
Ianina Nazarenko <iani.na.zarenko@gmail.com>
2026-01-05 21:14:11 +0300

    fix: Fix same origin urls validation in url-utils.ts

--- utils/helpers/url-utils.ts
+++ utils/helpers/url-utils.ts
@@ -1,3 +1,5 @@
+import { baseUrl } from '@/utils/constants/base-url';
+
 export function getSafeCallbackUrl(callbackUrl: string | undefined, fallback: string): string {
     if (!callbackUrl) {
         return fallback;
@@ -7,5 +9,11 @@ export function getSafeCallbackUrl(callbackUrl: string | undefined, fallback: st
         return callbackUrl;
     }
 
-    return fallback;
+    try {
+        const targetUrl = new URL(callbackUrl);
+        const appUrl = new URL(baseUrl);
+        return targetUrl.origin === appUrl.origin ? targetUrl.pathname + targetUrl.search : fallback;
+    } catch {
+        return fallback;
+    }
 }
