0f9f30bbd0a04244093cf31763e0b0a31bc6e78d
Ianina Nazarenko <iani.na.zarenko@gmail.com>
2026-01-07 16:20:49 +0300

    feat: Add a role prop to the user object for future RBAC

--- lib/auth.ts
+++ lib/auth.ts
@@ -28,6 +28,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
                 user: {
                     ...session.user,
                     id: user.id,
+                    role: user.role,
                 },
             };
         },
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,9 +1,21 @@
 import { type DefaultSession } from 'next-auth';
+import { ERole } from '@/utils/constants/users';
 
 declare module 'next-auth' {
     interface Session {
         user: {
             id: string;
+            role: ERole;
         } & DefaultSession['user'];
     }
+
+    interface User {
+        role?: ERole;
+    }
 }
+
+// declare module '@auth/core/adapters' {
+//     interface AdapterUser {
+//         role: ERole;
+//     }
+// }
