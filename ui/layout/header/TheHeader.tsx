da4042c11549b7972636bfe6deb42b4417b218e9
Ianina Nazarenko <iani.na.zarenko@gmail.com>
2026-01-08 18:23:14 +0300

    feat: Temp user pic animation

--- ui/layout/header/TheHeader.tsx
+++ ui/layout/header/TheHeader.tsx
@@ -19,8 +19,6 @@ function TheHeader({ session: serverSession }: { session?: Session | null }) {
     const isSignedIn = status === 'authenticated';
     // If serverSession is provided, we are never "loading" from the client's perspective
     const isLoading = status === 'loading' && !serverSession;
-    // UserPic should animate only if authentication happened on the client
-    const shouldAnimateUserPic = !serverSession && isSignedIn;
 
     return (
         <header className={s.header}>
@@ -36,7 +34,7 @@ function TheHeader({ session: serverSession }: { session?: Session | null }) {
                                 image={session.user.image}
                                 name={session.user.name}
                                 index={session.user.id}
-                                className={clsx(s.userPic, shouldAnimateUserPic && 'fade-in')}
+                                className={clsx(s.userPic, 'fade-in')}
                             />
                         </>
                     )}
