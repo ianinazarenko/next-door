9a6fb0f25b6191aec3ca190884468a051e31fc45
Ianina Nazarenko <iani.na.zarenko@gmail.com>
2026-01-08 12:52:15 +0300

    feat: env vars check for authentication

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,14 @@
+import 'server-only';
+
 import { PAGES } from '@/data/pages';
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { prisma } from '@/lib/data-access/db';
 import GitHub from 'next-auth/providers/github';
+
+if (!process.env.AUTH_GITHUB_ID || !process.env.AUTH_GITHUB_SECRET) {
+    throw new Error('Missing AUTH_GITHUB_ID or AUTH_GITHUB_SECRET');
+}
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
