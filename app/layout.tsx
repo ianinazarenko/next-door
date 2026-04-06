359b69ad9c0194cd7cbe97ddb033eb8efac2e30e
Ianina Nazarenko <iani.na.zarenko@gmail.com>
2026-04-06 23:52:22 +0300

    fix: Update font to one source of truth

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='en'>
-            <body className={`${inter.className} antialiased`}>
+            <body className={`${inter.variable} antialiased`}>
                 <AppProviders>
                     {children}
 
