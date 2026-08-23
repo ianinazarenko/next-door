6497f85964054952712121e911684d02cdf30725
Ianina Nazarenko <iani.na.zarenko@gmail.com>
2026-08-23 21:49:33 +0300

    fix: PostImage image improvements

--- app/(public)/posts/[id]/(components)/image/PostImage.tsx
+++ app/(public)/posts/[id]/(components)/image/PostImage.tsx
@@ -1,30 +1,20 @@
 import s from './PostImage.module.css';
 import Image from 'next/image';
-import clsx from 'clsx';
 
 interface IProps {
     image?: string | null;
 }
 
-const IMAGE_MOB = 'https://picsum.photos/600/400';
 const IMAGE_DESK = 'https://picsum.photos/1200/800';
 export default function PostImage({ image }: IProps) {
     return (
         <div>
             <div className={s.container}>
                 <Image
-                    src={image || IMAGE_MOB}
-                    sizes='100vw'
-                    alt='Post image'
-                    className={clsx(s.image, s.mob)}
-                    fill
-                />
-
-                <Image
                     src={image || IMAGE_DESK}
-                    sizes='100vw'
+                    sizes='(min-width: 1024px) 800px, 100vw'
                     alt='Post image'
-                    className={clsx(s.image, s.desk)}
+                    className={s.image}
                     fill
                 />
 
